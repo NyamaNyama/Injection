614e7199febc631a378f596deae867a198682514
NyamaNyama <tihon31.01.04@mail.ru>
2025-03-27 14:05:52 +1000

    Add ghost prefab and GhostDisappearence script

---
+++ Assets/Game/Scripts/ItemBehavior/GhostDisappearence.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+namespace Game.Scripts.ItemBehavior
+{
+    public class GhostDisappearence : MonoBehaviour
+    {
+        [SerializeField] private float ghostLifeTime;
+        private SpriteRenderer _sprite;
+        private float _timeLeft;
+        
+
+        private void Start()
+        {
+            _timeLeft = ghostLifeTime;
+            _sprite = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            Color spritecolor = _sprite.color;
+            spritecolor.a =  Mathf.Clamp(_timeLeft / ghostLifeTime, 0, 1);
+            _sprite.color = spritecolor;
+            if (_sprite.color.a == 0)
+            {
+                Destroy(gameObject);
+            }
+            _timeLeft -= Time.deltaTime;
+        }
+    }
+}
