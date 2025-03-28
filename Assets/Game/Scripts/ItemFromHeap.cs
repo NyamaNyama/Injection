085d4529b1af44451edc0b4153e9df3b2e67150d
NyamaNyama <tihon31.01.04@mail.ru>
2025-03-28 22:09:58 +1000

    Add shelves and Dragon animation

--- Assets/Game/Scripts/ItemFromHeap.cs
+++ Assets/Game/Scripts/ItemFromHeap.cs
@@ -11,18 +11,18 @@ namespace Game.Scripts
         private Vector2 _spawnPos;
         public static List<ItemObject> activeItems = new List<ItemObject>();
         private const int MaxItemCount = 10;
+        [SerializeField] private Transform spawnPoint;
 
         private void Start()
         {
             Collider2D collider= GetComponent<Collider2D>();
-            _spawnPos = new Vector2(collider.bounds.min.x, collider.bounds.max.y / 2);
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
             if (activeItems.Count < MaxItemCount )
             {
-                activeItems.Add(Instantiate(spawnObject, _spawnPos, Quaternion.identity));
+                activeItems.Add(Instantiate(spawnObject, spawnPoint.position, Quaternion.identity));
             }
         }
 
